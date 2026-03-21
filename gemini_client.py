c5c6c98619dd0ff3c625bee2d24010062026572f
Jonathan Pina-V <jon.pina25@gmail.com>
2026-03-21 15:04:42 -0700

    API key hidden

--- gemini_client.py
+++ gemini_client.py
@@ -1,8 +1,9 @@
 from google import genai
 from google.genai import types
+import os
 
 # Initialize Gemini client #
-client = genai.Client(api_key = "")
+client = genai.Client(api_key = os.environ.get("GEMINI_API"))
 MODEL_ID = "gemini-3-flash-preview"
 SYS_INSTRUCTION = "At the end of every prompt, indicate how must energy and water your response consumed."
 
