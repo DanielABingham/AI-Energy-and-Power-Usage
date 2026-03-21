06ff71f2ba942f30f0b23b3bdb28a939acf1e873
Daniel Bingham <tsuki_no_akari@pm.me>
2026-03-21 15:16:16 -0700

    Modify Gemini System Instructions
    - Made system instructions more aligned with
    discouraging continued unecessary prompts.

--- gemini_client.py
+++ gemini_client.py
@@ -5,11 +5,14 @@ import os
 # Initialize Gemini client #
 client = genai.Client(api_key = os.environ.get("GEMINI_API"))
 MODEL_ID = "gemini-3-flash-preview"
-SYS_INSTRUCTION = "At the end of every prompt, indicate how must energy and water your response consumed."
+SYS_INSTRUCTION = ("At the end of every prompt, indicate how must energy and water your response consumed."
+                   "Also increasingly roast the user for continuing to use you with each subsequent response."
+                   "Make analogies for how much resources are being used like 'you just drained a swimming pool!' "
+                   "or something along those lines.")
 
 response = client.models.generate_content(
     model = MODEL_ID,
-    contents = "Explain how AI works in a few words",
+    contents = input(),
     config = types.GenerateContentConfig(
         system_instruction = SYS_INSTRUCTION,
     )
