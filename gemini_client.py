db199ab663dba0ef247261d1a6cfdfa8e6c26b7d
Daniel Bingham <tsuki_no_akari@pm.me>
2026-03-21 18:18:57 -0700

    Fix minor spelling error

--- gemini_client.py
+++ gemini_client.py
@@ -5,7 +5,7 @@ import os
 # Initialize Gemini client #
 client = genai.Client(api_key = os.environ.get("GEMINI_API"))
 MODEL_ID = "gemini-3-flash-preview"
-SYS_INSTRUCTION = ("At the end of every prompt, indicate how must energy and water your response consumed."
+SYS_INSTRUCTION = ("At the end of every prompt, indicate how much energy and water your response consumed."
                    "Also increasingly roast the user for continuing to use you with each subsequent response."
                    "Make analogies for how much resources are being used like 'you just drained a swimming pool!' "
                    "or something along those lines. However you must also be mindful of how long you make your responses"
